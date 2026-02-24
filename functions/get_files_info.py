3c65a07a79804b3b769092ab62e312ceed91040f
bmahne812024 <bmahne81@gmail.com>
2026-02-23 22:57:44 -0500

    initial commit

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        absolute_path = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(absolute_path, directory))
+        valid_target_dir = os.path.commonpath([absolute_path, target_dir]) == absolute_path
+
+        if valid_target_dir == False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+
+            files_info.append(f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}")
+        return "\n".join(files_info)
+
+    except Exception as e:
+        return f"Error: {e}"
+
